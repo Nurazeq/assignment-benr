1f22db7c9433a34b33a4dec343de9099834cfd29
Zukir16 <b022210146@student.utem.edu.my>
2025-01-21 00:20:44 +0800

    Initial Test

---
+++ server.mjs
@@ -0,0 +1,38 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+const SECRET_KEY = '6LeiXr0qAAAAAFz8UjMt2cwY-C7hW1O_1TYb7EpG'; // Replace with your actual secret key
+
+app.use(express.json());
+app.use(cors());
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to handle CAPTCHA verification
+app.post('/verify-captcha', async (req, res) => {
+    const captchaResponse = req.body.captchaResponse;
+
+    const googleResponse = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
+        method: 'POST',
+        body: new URLSearchParams({
+            secret: SECRET_KEY,
+            response: captchaResponse
+        })
+    });
+
+    const verificationResult = await googleResponse.json();
+
+    if (verificationResult.success) {
+        res.json({ message: 'reCAPTCHA verified successfully!' });
+    } else {
+        res.status(400).json({ message: 'reCAPTCHA verification failed!' });
+    }
+});
+
+app.listen(process.env.PORT || 80, () => {
+    console.log('Server running on port 80');
+});
